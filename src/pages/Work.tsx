6472daed9837029af885106294707711d85e499b
Meet <meetaparekh2003@gmail.com>
2026-04-10 03:41:50 +0530

    ..

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -2,7 +2,7 @@ import { motion } from "motion/react";
 import { Link } from "react-router-dom";
 import { Leaf, Shield, CheckCircle } from "lucide-react";
 import Image1 from "../../assets/newimage1.jpg";
-import Image2 from "../../assets/image2.jpg";
+import Image2 from "../../assets/newimage2.jpg";
 import Image3 from "../../assets/image3.jpg";
 import Image4 from "../../assets/image4.webp";
 import Image5 from "../../assets/image5.jpg";
