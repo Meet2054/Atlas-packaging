f659ec3bc6c847b95bde4513d99f3742438265d3
Meet <meetaparekh2003@gmail.com>
2026-04-10 03:15:05 +0530

    .

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,42 +1,42 @@
 import { motion } from "motion/react";
 import { Link } from "react-router-dom";
 import { Leaf, Shield, CheckCircle } from "lucide-react";
-
+import Image1 from "../../assets/Image1.jpg";
 const projects = [
   {
     category: "Luxury Cosmetics",
     title: "Onyx Essence Collection",
-    image: "./assets/Image1.jpg",
+    image: Image1,
     aspect: "aspect-[4/5]"
   },
   {
     category: "Eco-friendly Apparel",
     title: "Sustainable Basics Shipper",
-    image: "./assets/Image2.jpg",
+    image: "../../assets/Image2.jpg",
     aspect: "aspect-square"
   },
   {
     category: "Artisan Coffee",
     title: "Roaster's Reserve Tin",
-    image: "./assets/image3.jpg",
+    image: "../../assets/image3.jpg",
     aspect: "aspect-[3/4]"
   },
   {
     category: "Premium Tech",
     title: "Vector Audio Series",
-    image: "./assets/image4.webp",
+    image: "../../assets/image4.webp",
     aspect: "aspect-square"
   },
   {
     category: "Specialty Skincare",
     title: "Molecular Repair Kit",
-    image: "./assets/image7.jpg",
+    image: "../../assets/image7.jpg",
     aspect: "aspect-[4/5]"
   },
   {
     category: "Artisan Food",
     title: "Heirloom Confectionery",
-    image: "./assets/image5.jpg",
+    image: "../../assets/image5.jpg",
     aspect: "aspect-[3/2]"
   }
 ];
